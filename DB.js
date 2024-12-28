a9840fbc79dd7d4c8b03b3c5b20f022c8c2a2b04
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-12-28 22:18:46 +0530

    update userschema

--- DB.js
+++ DB.js
@@ -3,14 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-const userSchema = new Schema({
-  email: { type: String, unique: true },
-  password: String,
-  firstName: String,
-  lastName: String,
-  userID: ObjectId,
-});
-
 const todoSchema = new Schema({
   title: String,
   description: String,
@@ -20,18 +12,16 @@ const todoSchema = new Schema({
     enum: ["Low", "Medium", "High"],
     default: "Medium",
   },
-  dueDate: {
-    type: Date,
-    required: true,
-    validate: {
-      validator: function (value) {
-        return value instanceof Date && !isNaN(value);
-      },
-      message: "Invalid date format.",
-    },
-  },
 });
 
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+  userID: ObjectId,
+  todos: [todoSchema],
+});
 const todoModel = mongoose.model("todos", todoSchema);
 const userModel = mongoose.model("users", userSchema);
 
