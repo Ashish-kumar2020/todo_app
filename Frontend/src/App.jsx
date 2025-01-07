71bf1a7fd2a3dc281e94be9b847bf42e487b7c05
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-01-07 23:14:04 +0530

    created sidenavbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import SideNav from "./components/SideNav";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 
@@ -8,6 +9,7 @@ function App() {
     <>
       {/* <Signup /> */}
       <Header />
+      <SideNav />
       {/* <Signin /> */}
     </>
   );
---
+++ Frontend/src/components/SideNav.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import Profile from "../assets/profile.png";
+
+const SideNav = () => {
+  const [activeItem, setActiveItem] = useState("Dashboard");
+
+  const navItems = [
+    { id: "Dashboard", label: "Dashboard" },
+    { id: "Vital Task", label: "Vital Task" },
+    { id: "My Task", label: "My Task" },
+    { id: "Task Categories", label: "Task Categories" },
+    { id: "Settings", label: "Settings" },
+    { id: "Help", label: "Help" },
+  ];
+
+  return (
+    <div className="w-[315px] h-[100vh] bg-customRed rounded-tr-[10px] rounded-br-[10px]">
+      {/* Profile image */}
+      <div className="relative left-[107px] top-[20px]">
+        <img src={Profile} alt="Profile" />
+      </div>
+      {/* User Details */}
+      <div className="relative left-[74px] w-[176px] h-[36px] top-[30px]">
+        <h1 className="text-[16px] font-bold text-white">Ashish Kumar Singh</h1>
+        <h1 className="text-[12px] text-white">ashishsingh@upnext.com</h1>
+      </div>
+
+      {/* Navigation List */}
+      <ul className="relative top-[50px]">
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => {
+              setActiveItem(item.id);
+            }}
+            className={`w-[258px] h-[59px] ml-[21px] cursor-pointer text-[16px] font-inter mb-[12px] flex items-center rounded-[15px] ${
+              activeItem === item.id ? "bg-white text-customRed" : "text-white"
+            }`}
+          >
+            {" "}
+            <span className="ml-[16px]">{item.label}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SideNav;
